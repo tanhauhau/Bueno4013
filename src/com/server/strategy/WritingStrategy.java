e69b6cd250820d9031bd22e4d1c72d6afb69e3b2
Bert Khoo <khoogilbert@gmail.com>
2016-03-28 20:25:05 +0800

    writing Strategy comment done

--- src/com/server/strategy/WritingStrategy.java
+++ src/com/server/strategy/WritingStrategy.java
@@ -9,11 +9,11 @@ import java.io.RandomAccessFile;
 
 /**
  * Created by lhtan on 22/3/16.
+ *
+ * This class extend the Parent Class Strategy
+ * This class is incharge of handling writing request from client
  */
-/*
-    Receive the content that the client want to write into a
-    certain file and reply the result to the client
- */
+
 
 public class WritingStrategy extends Strategy {
     private String folder = "";
@@ -22,6 +22,13 @@ public class WritingStrategy extends Strategy {
     private final static String FILENAME = "filename";
     private final static String OFFSET = "offset";
     private final static String DATA = "data";
+
+    /**
+     * Class Constructor for WritingStrategy
+     *
+     * @param folder
+     * @param callback
+     */
 
     public WritingStrategy(String folder, Callback callback) {
         super(new Unpack.Builder()
@@ -33,6 +40,17 @@ public class WritingStrategy extends Strategy {
         this.callback = callback;
     }
 
+    /**
+     * handle method inherited from Parent Class Strategy and
+     * mandatory to implement
+     *
+     * Find the file (if available) and write the content into it
+     * otherwise error message is returned
+     *
+     * @param request
+     * @return result message
+     * @throws IOException
+     */
     @Override
     protected byte[] handle(Request request) throws IOException {
         Unpack.Result result = request.getData();
@@ -40,25 +58,25 @@ public class WritingStrategy extends Strategy {
         Integer offset = result.getInt(OFFSET);
         String data = result.getString(DATA);
 
-        if (ifAnyNull(filename, offset, data))   return replyError(request.getRequestID(), "Corrupted data");
+        if (ifAnyNull(filename, offset, data))
+            return replyError(request.getRequestID(), "Corrupted data");    /* Check is there any null object in parameter */
 
         File file = new File(folder, filename);
         if (!file.exists()){
-            return replyError(request.getRequestID(), "No such file");
+            return replyError(request.getRequestID(), "No such file");      /* Return error message if file not found */
         }else {
             long fileByteSize = file.length();
             if (offset > fileByteSize) {
-                return replyError(request.getRequestID(), "Offset too big!!! ");
+                return replyError(request.getRequestID(), "Offset too big!!! ");    /* Return error message if offset exceed the size of the file */
             } else {
                 System.out.println(String.format("  Writing Strategy >> Write %s with offset=%d data=%s", filename, offset, data));
                 RandomAccessFile raf = new RandomAccessFile(file, "rw");
-                raf.seek(offset);
-                raf.writeBytes(data);
-                raf.close();
-                //inform
-                callback.inform(filename, request.getSocket());
+                raf.seek(offset);           /* move the pointer of the file to the offset location */
+                raf.writeBytes(data);       /* write the content into the file */
+                raf.close();                /* close file */
+                callback.inform(filename, request.getSocket());     /* inform client who registered for callback about the changes of file */
 
-                return replySuccess(request.getRequestID(), "Write Successful");
+                return replySuccess(request.getRequestID(), "Write Successful");    /* Return success message */
             }
         }
     }
